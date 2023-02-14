93b22807e40111c11b578b10dfeb5baf84b95148
Mieu <mieuhamchoi@gmail.com>
2023-02-14 14:52:33 +0700

    carousel freature finish

--- src/app/modules/carousel/carousel.module.ts
+++ src/app/modules/carousel/carousel.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './carousel.component';
 
-
-
-
 @NgModule({
   declarations: [
     CarouselComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [CarouselComponent]
 })
 export class CarouselModule { }
