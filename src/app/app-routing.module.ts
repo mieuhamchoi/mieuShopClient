98504442fed6bfef6eea81e7bf5a080f2fcfffc1
Mieu <mieuhamchoi@gmail.com>
2023-02-14 17:01:20 +0700

    learning module, lazy loading module use router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', 
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
